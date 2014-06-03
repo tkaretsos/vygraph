ebe6e5db66034c37d29e3a84eb61a6739d7cd7e0
Thanasis Karetsos <karetsosjr@gmail.com>
2014-06-03 22:11:11 +0300

    redefine vygraph lang macros

--- inputs/vygraph_lang.h
+++ inputs/vygraph_lang.h
@@ -1,17 +1,20 @@
 #ifndef VYGRAPH_LANG_H
 #define VYGRAPH_LANG_H
 
-#define vy_decl_function(name) \
-\
-void spawn_thread_##name();\
-\
-void join_thread_##name();
-/* end vy_decl_function macro */
+#ifdef __clang__
+  void vy_spawn(...) __attribute__((overloadable));
+  void vy_join(...) __attribute__((overloadable));
+#elif __GNUC__
+  #define vy_spawn(name, ...)                                                  \
+    void vy_spawn_##name() __attribute__((weak));                              \
+    vy_spawn_##name(__VA_ARGS__);
 
-#define vy_spawn(function, ...) spawn_thread_##function();
-#define vy_join(function, ...) join_thread_##function();
+  #define vy_join(name, ...)                                                   \
+    void vy_join_##name() __attribute__((weak));                               \
+    vy_join_##name(__VA_ARGS__);
+#endif
 
-void vy_atomic_begin();
-void vy_atomic_end();
+void vy_atomic_begin() __attribute__((weak));
+void vy_atomic_end() __attribute__((weak));
 
 #endif /* VYGRAPH_LANG_H */
