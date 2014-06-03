5ed6c9e0fa9600494d4713e5f79f7bf89c2735cf
Thanasis Karetsos <karetsosjr@gmail.com>
2014-06-03 22:12:34 +0300

    add more testing cases

--- inputs/test.c
+++ inputs/test.c
@@ -1,15 +1,21 @@
 #include "vygraph_lang.h"
 
-vy_decl_function(add)
+void sub() {
+  int a = 2, b = 3;
+  a += 3;
+  b += a;
+}
 
 void add(int s) {
-    int a = 2, b = 3;
-    a += 3;
-    b += a;
+  int a = 2, b = 3;
+  a += 3;
+  b += a;
 }
 
 int main() {
-  vy_spawn(add);
+  vy_spawn(sub);
+  vy_spawn(add, 5);
+  vy_join(sub);
   vy_join(add);
   vy_atomic_begin();
   add(5);
