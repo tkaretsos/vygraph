e00f5f7450350b1eb47c0b0b87c1d17d6cd06b41
Thanasis Karetsos <karetsosjr@gmail.com>
2014-09-18 21:03:25 +0200

    refacter inline visitor

--- Inline/InlineASTVisitor.cc
+++ Inline/InlineASTVisitor.cc
@@ -14,62 +14,70 @@ InlineASTVisitor::InlineASTVisitor(Rewriter& rewriter, ASTContext& context)
 }
 
 bool InlineASTVisitor::VisitCallExpr(CallExpr* call) {
-  FunctionDecl* funcDecl = call->getDirectCallee();
-  if (functionMgr.isUserDefined(funcDecl->getNameAsString())) {
-    CompoundStmt* funcBody = cast<CompoundStmt>(funcDecl->getBody());
-
-    // eg: void foo() { }
-    // *code* ...
-    // foo();
-    // *code* ...
-    if (call->getNumArgs() == 0 && functionMgr.isSimpleCall(call)) {
-
-      // delete the call text
-      rewriter.RemoveText(call->getSourceRange());
-      rewriter.RemoveText(call->getLocStart(), 1); // the semicolon
-
-      for (auto i = funcBody->body_begin(); i != funcBody->body_end(); i++) {
-        if (!isa<ReturnStmt>(*i)) {
-          rewriter.InsertText(call->getLocStart(),
-                              rewriter.ConvertToString(*i), true, true);
-          if (isa<Expr>(*i)) {
-            rewriter.InsertText(call->getLocStart(), ";\n", true, true);
-          }
-        }
+  if (functionMgr.isUserDefined(call->getDirectCallee()->getNameAsString())) {
+    if (call->getNumArgs() == 0) {
+      if (functionMgr.isSimpleCall(call)) {
+        handleSimpleCallNoArgs(call);
+      } else {
+        handleNoArgs(call);
       }
-    }
-
-    // eg: <nonvoid> foo() { ... return <expr>; }
-    // *code* ...
-    // x = <expr> <operator> foo() <operator> <expr>;
-    // *code* ...
-    if (call->getNumArgs() == 0 && !functionMgr.isSimpleCall(call)) {
-      string varName(call->getDirectCallee()->getNameAsString() + "_retval");
-      auto startLoc = functionMgr.getStmtLoc(call);
-      auto retType = call->getCallReturnType();
-
-      for (auto i = funcBody->body_begin(); i != funcBody->body_end(); i++) {
-        if (!isa<ReturnStmt>(*i)) {
-          rewriter.InsertText(startLoc,
-                              rewriter.ConvertToString(*i), true, true);
-          if (isa<Expr>(*i)) {
-            rewriter.InsertText(startLoc, ";\n", true, true);
-          }
-        } else {
-          auto retStmt = cast<ReturnStmt>(*i);
-          auto retVal = rewriter.ConvertToString(*retStmt->children().first);
-          string newStr("");
-          newStr += retType.getAsString() + " ";
-          newStr += varName + " = ";
-          newStr += retVal + ";\n";
-          rewriter.InsertText(startLoc, newStr, true, true);
-        }
-      }
-      rewriter.ReplaceText(call->getSourceRange(), varName);
     }
   }
 
   return true;
 }
+
+// eg: void foo() { }
+// *code* ...
+// foo();
+// *code* ...
+void InlineASTVisitor::handleSimpleCallNoArgs(clang::CallExpr* call) const {
+  CompoundStmt* funcBody = cast<CompoundStmt>(call->getDirectCallee()->getBody());
+
+  // delete the call text
+  rewriter.RemoveText(call->getSourceRange());
+  rewriter.RemoveText(call->getLocStart(), 1); // the semicolon
+
+  for (auto i = funcBody->body_begin(); i != funcBody->body_end(); i++) {
+    if (!isa<ReturnStmt>(*i)) {
+      rewriter.InsertText(call->getLocStart(),
+                          rewriter.ConvertToString(*i), true, true);
+      if (isa<Expr>(*i)) {
+        rewriter.InsertText(call->getLocStart(), ";\n", true, true);
+      }
+    }
+  }
+}
+
+// eg: <nonvoid> foo() { ... return <expr>; }
+// *code* ...
+// x = <expr> <operator> foo() <operator> <expr>;
+// *code* ...
+void InlineASTVisitor::handleNoArgs(CallExpr* call) const {
+  CompoundStmt* funcBody = cast<CompoundStmt>(call->getDirectCallee()->getBody());
+  string varName(call->getDirectCallee()->getNameAsString() + "_retval");
+  auto startLoc = functionMgr.getStmtLoc(call);
+  auto retType = call->getCallReturnType();
+
+  for (auto i = funcBody->body_begin(); i != funcBody->body_end(); i++) {
+    if (!isa<ReturnStmt>(*i)) {
+      rewriter.InsertText(startLoc,
+                          rewriter.ConvertToString(*i), true, true);
+      if (isa<Expr>(*i)) {
+        rewriter.InsertText(startLoc, ";\n", true, true);
+      }
+    } else {
+      auto retStmt = cast<ReturnStmt>(*i);
+      auto retVal = rewriter.ConvertToString(*retStmt->children().first);
+      string newStr("");
+      newStr += retType.getAsString() + " ";
+      newStr += varName + " = ";
+      newStr += retVal + ";\n";
+      rewriter.InsertText(startLoc, newStr, true, true);
+    }
+  }
+  rewriter.ReplaceText(call->getSourceRange(), varName);
+}
+
 
 } // namespace vy
--- Inline/InlineASTVisitor.hh
+++ Inline/InlineASTVisitor.hh
@@ -10,6 +10,10 @@ class InlineASTVisitor : public clang::RecursiveASTVisitor<InlineASTVisitor> {
   clang::Rewriter& rewriter;
   clang::Rewriter::RewriteOptions opts;
   clang::ASTContext& context;
+
+  void handleSimpleCallNoArgs(clang::CallExpr*) const;
+  void handleNoArgs(clang::CallExpr*) const;
+
 public:
   InlineASTVisitor(clang::Rewriter&, clang::ASTContext&);
   bool VisitCallExpr(clang::CallExpr*);
