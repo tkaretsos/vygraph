24e86c32f90649b066461f778f2b7967c6cab270
Thanasis Karetsos <karetsosjr@gmail.com>
2014-09-23 19:55:22 +0200

    add test function that returns float

--- inputs/one_arg_with_ret.c
+++ inputs/one_arg_with_ret.c
@@ -4,9 +4,16 @@ int foo(int x) {
   return y;
 }
 
+float bar(float x, float y) {
+  float z = x * y;
+  return z + 1;
+}
+
 int main() {
   int a = 5;
-  a = foo(5);
+  a = 5 + foo(a)  * 6;
+
+  float f = 1.0f + bar(3.f + 4.f, 6.f) * 3;
 
   return 0;
 }
