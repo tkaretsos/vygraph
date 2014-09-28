5eb470372b0c2eadaec72e3fe2bb621b86fdd51c
Thanasis Karetsos <karetsosjr@gmail.com>
2014-09-28 18:29:08 +0200

    inline functions with args and ret value

--- Inline/InlineASTVisitor.cc
+++ Inline/InlineASTVisitor.cc
@@ -124,9 +124,8 @@ void InlineASTVisitor::handleSimpleCallWithArgs(CallExpr* call) const {
       }
     }
 
-    if (isa<Expr>(*s)) {
+    if (isa<Expr>(*s))
       insertStr.append(";\n");
-    }
     rewriter.InsertText(call->getLocStart(), insertStr, true, true);
   }
 }
@@ -136,73 +135,52 @@ void InlineASTVisitor::handleSimpleCallWithArgs(CallExpr* call) const {
 // x = <expr> <operator> foo(arg1, ...) <operator> <expr>;
 // *code* ...
 void InlineASTVisitor::handleArgs(CallExpr* call) const {
-  CompoundStmt* funcBody = cast<CompoundStmt>(call->getDirectCallee()->getBody());
-  string varName(call->getDirectCallee()->getNameAsString() + "_retval");
-  auto startLoc = functionMgr.getStmtLoc(call);
-  auto retType = call->getCallReturnType();
-  map<string, Expr*> varMap;
-  map<Stmt*, string> rollbacks;
+  string callReplacement("(");
 
+  // create a map between the parameters of the function
+  // and the arguments of the call
+  map<string, Expr*> varMap;
   auto param = call->getDirectCallee()->param_begin();
   for (auto arg = call->arg_begin(); arg != call->arg_end(); ++arg, ++param) {
     varMap[(*param)->getNameAsString()] = *arg;
   }
 
-//   for (auto v : varMap) {
-//     std::cout << v.first << " to " << rewriter.ConvertToString(v.second) << std::endl;
-//   }
-  for (auto s = funcBody->body_begin(); s != funcBody->body_end(); ++s) {
-    rollbacks[*s] = rewriter.ConvertToString(*s);
-    if (isa<ReturnStmt>(*s)) {
-      rollbacks[*s].erase(rollbacks[*s].end() - 2, rollbacks[*s].end());
-    } else {
-      rollbacks[*s].pop_back();
-    }
-
+  auto body = cast<CompoundStmt>(call->getDirectCallee()->getBody());
+  for (auto s = body->body_begin(); s != body->body_end(); ++s) {
+    string insertStr = rewriter.ConvertToString((*s));
     vector<DeclRefExpr*> declRefs;
     findRefInStmt(*s, declRefs);
+    reverse(declRefs.begin(), declRefs.end());
     for (auto declRef : declRefs) {
       auto found = varMap.find(declRef->getFoundDecl()->getNameAsString());
       if (found != varMap.end()) {
-        rewriter.ReplaceText(declRef->getSourceRange(),
-                             "(" + rewriter.ConvertToString(found->second) + ")");
+        auto offset = declRef->getLocStart().getRawEncoding() -
+                      (*s)->getLocStart().getRawEncoding();
+        auto begin = insertStr.begin() + offset;
+        string exprStr("(" + rewriter.ConvertToString(found->second) + ")");
+        insertStr.replace(begin, begin + found->first.length(), exprStr);
       }
     }
 
-    if (!isa<ReturnStmt>(*s)) {
-      string newText(rewriter.getRewrittenText((*s)->getSourceRange()));
-      if (isa<Expr>(*s)) {
-        newText.push_back(';');
-      }
-      newText.push_back('\n');
-      rewriter.InsertText(startLoc, newText, true, true);
+    if (isa<ReturnStmt>(*s)) {
+      callReplacement.append(insertStr.begin() + 7, insertStr.end() - 2);
+      callReplacement.push_back(')');
     } else {
-      auto retStmt = cast<ReturnStmt>(*s);
-      auto retVal = rewriter.ConvertToString(*retStmt->children().first);
-      string newStr("");
-      newStr += retType.getAsString() + " ";
-      newStr += varName + " = ";
-      newStr += retVal + ";\n";
-      rewriter.InsertText(startLoc, newStr, true, true);
+      if (isa<Expr>(*s))
+        insertStr.append(";\n");
+      rewriter.InsertText(functionMgr.getStmtLoc(call), insertStr, true, true);
     }
   }
-  rewriter.ReplaceText(call->getSourceRange(), varName);
-
-  for (auto r : rollbacks) {
-    rewriter.ReplaceText(r.first->getSourceRange(), r.second);
-  }
+  rewriter.ReplaceText(call->getSourceRange(), callReplacement);
 }
 
 
 void InlineASTVisitor::findRefInStmt(Stmt* stmt, vector<DeclRefExpr*>& declRefs) const {
-
-  if (DeclRefExpr* ref = dyn_cast<DeclRefExpr>(stmt)) {
+  if (DeclRefExpr* ref = dyn_cast<DeclRefExpr>(stmt))
     declRefs.push_back(ref);
-  }
 
-  for (auto c : stmt->children()) {
+  for (auto c : stmt->children())
     findRefInStmt(c, declRefs);
-  }
 }
 
 
