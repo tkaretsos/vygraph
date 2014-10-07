ced3d8cd36db08c473bd058f001441f7d57e69f8
Thanasis Karetsos <karetsosjr@gmail.com>
2014-10-07 22:02:34 +0200

    add readers writers example

---
+++ inputs/readers_writers.c
@@ -0,0 +1,48 @@
+#include "vygraph_lang.h"
+#include <stdbool.h>
+
+bool lock = true;
+bool writing = false;
+int i = 0;
+
+void writer() {
+  writing = true;
+  writing = false;
+}
+
+void reader() {
+  assert(!writing);
+}
+
+void increment_i() {
+  i = i + 1;
+}
+
+int main () {
+  vy_atomic_begin();
+//     assume(lock);
+  lock = false;
+  vy_spawn(writer);
+  vy_atomic_end();
+
+  vy_atomic_begin();
+  vy_join(writer);
+  lock = true;
+  vy_atomic_end();
+
+  if (i == 0) {
+    vy_atomic_begin();
+    increment_i();
+//       assume(lock);
+    lock= false;
+    vy_spawn(reader);
+    vy_atomic_end();
+  } else if (i >= 1) {
+    vy_atomic_begin();
+    increment_i();
+    vy_spawn(reader);
+    vy_atomic_end();
+  }
+
+  return 0;
+}
