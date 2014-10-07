6edaaa2812779f54a68246916aedb7033ce5689c
Thanasis Karetsos <karetsosjr@gmail.com>
2014-10-07 19:51:52 +0200

    add TranslateAction class

---
+++ Translate/TranslateAction.cc
@@ -0,0 +1,42 @@
+#include "TranslateAction.hh"
+
+#include <fstream>
+
+#include "TranslateASTConsumer.hh"
+
+namespace vy {
+
+using namespace clang;
+
+TranslateAction::TranslateAction() { }
+
+ASTConsumer*
+TranslateAction::CreateASTConsumer(CompilerInstance& CI, llvm::StringRef file) {
+  std::cout << "CreateASTConsumer" << std::endl;
+  return new TranslateASTConsumer(CI.getASTContext(), outStream);
+}
+
+bool
+TranslateAction::BeginSourceFileAction(CompilerInstance& CI, StringRef filename) {
+  std::cout << "BeginSourceFileAction" << " = " << filename.str() << std::endl;
+  outFileName = filename.str();
+  auto found = outFileName.find_last_of('.');
+  if (found != std::string::npos)
+    outFileName.replace(outFileName.begin() + found, outFileName.end(), ".vyc");
+  else
+    outFileName.append(".vyc");
+  return true;
+}
+
+void
+TranslateAction::EndSourceFileAction() {
+  std::cout << "EndSourceFileAction" << std::endl;
+  std::ofstream outFile;
+  outFile.open(outFileName, std::ios_base::trunc);
+  outFile << outStream.rdbuf() << std::endl;
+  outFile.flush();
+  outFile.close();
+}
+
+
+} // namespace vy
---
+++ Translate/TranslateAction.hh
@@ -0,0 +1,32 @@
+#ifndef VYGRAPH_TRANSLATEACTION_HH
+#define VYGRAPH_TRANSLATEACTION_HH
+
+#include <sstream>
+
+#include "llvm/ADT/StringRef.h"
+#include "clang/AST/ASTConsumer.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/FrontendAction.h"
+
+namespace vy {
+
+class TranslateAction : public clang::ASTFrontendAction {
+public:
+  TranslateAction();
+
+protected:
+  clang::ASTConsumer* CreateASTConsumer(clang::CompilerInstance&,
+                                        llvm::StringRef) override;
+
+  bool BeginSourceFileAction(clang::CompilerInstance&, llvm::StringRef) override;
+
+  void EndSourceFileAction() override;
+
+private:
+  std::stringstream outStream;
+  std::string outFileName;
+};
+
+} // namespace vy
+
+#endif // VYGRAPH_TRANSLATEACTION_HH
