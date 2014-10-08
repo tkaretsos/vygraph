be38930aff14a28cca4e95cddf1d03ed2873c51b
Thanasis Karetsos <karetsosjr@gmail.com>
2014-10-08 17:20:07 +0200

    add Functions.cc file

--- Utility/Functions.hh
+++ Utility/Functions.hh
@@ -5,35 +5,13 @@
 
 #include "clang/AST/ASTContext.h"
 #include "clang/Basic/SourceLocation.h"
-#include "clang/Basic/SourceManager.h"
-#include "clang/Lex/Lexer.h"
 
 namespace vy {
 namespace util {
 
-std::string
-RangeToStr(const clang::SourceRange& range, const clang::ASTContext& context) {
-  auto& SM = context.getSourceManager();
-  auto charRange = clang::CharSourceRange::getTokenRange(range);
-  auto beginLoc = SM.getSpellingLoc(charRange.getBegin());
-  auto endLoc = SM.getSpellingLoc(charRange.getEnd());
-  auto size = SM.getDecomposedLoc(endLoc).second -
-              SM.getDecomposedLoc(beginLoc).second;
-  size += clang::Lexer::MeasureTokenLength(endLoc, SM, context.getLangOpts());
+std::string RangeToStr(const clang::SourceRange&, const clang::ASTContext&);
 
-  return std::string(SM.getCharacterData(beginLoc), size);
-}
-
-std::string
-random_alphanum(size_t length = 3) {
-  const char charset[] = "0123456789"
-                         "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                         "abcdefghijklmnopqrstuvwxyz";
-  std::string str(length, 0);
-  auto randchar = [&]() { return charset[rand() % (sizeof(charset) - 1)]; };
-  generate_n(str.begin(), length, randchar);
-  return str;
-}
+std::string random_alphanum(size_t = 3);
 
 } // namespace util
 } // namespace vy
