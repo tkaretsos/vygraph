da50c09d2e22d46d0bd4a2543cf9db727025e44e
Thanasis Karetsos <karetsosjr@gmail.com>
2014-10-17 14:40:28 +0200

    add if statement

--- inputs/translate/sequential.c
+++ inputs/translate/sequential.c
@@ -1,5 +1,17 @@
+#include "../vygraph_lang.h"
+
+int y = 1;
+int k;
+
 int main () {
+  bool b = false;
+  bool a = true;
+  bool c;
   int x = 0;
-  x = x + 3;
+  if (x == 0) {
+    x = x + 3;
+    x++;
+  }
+  x--;
   return 0;
 }
