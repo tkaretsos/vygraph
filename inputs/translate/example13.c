b669fea8bcc3ffca2431223287d28d0233627463
Thanasis Karetsos <karetsosjr@gmail.com>
2014-10-28 20:40:13 +0100

    add example inputs

--- inputs/translate/example13.c
+++ inputs/translate/example13.c
@@ -2,11 +2,9 @@ int main() {
   int x = 0;
   int y = 0;
   while (x > 5) {
-    if (y <= 20) {
-      while (x <= y) {
-        x++;
-        y--;
-      }
+    while (x <= y) {
+      x++;
+      y--;
     }
   }
   x--;
