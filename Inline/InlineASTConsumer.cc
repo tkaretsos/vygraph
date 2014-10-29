d8cdf8b35f37962dfe475ebdcc5f5a0f8c07c5c3
Thanasis Karetsos <karetsosjr@gmail.com>
2014-10-29 17:25:51 +0100

    rename member variable

--- Inline/InlineASTConsumer.cc
+++ Inline/InlineASTConsumer.cc
@@ -5,13 +5,13 @@ namespace vy {
 using namespace clang;
 
 InlineASTConsumer::InlineASTConsumer(Rewriter& rewriter, ASTContext& context)
-  : inliner(rewriter, context)
+  : inlineVisitor(rewriter, context)
 { }
 
 bool
 InlineASTConsumer::HandleTopLevelDecl(DeclGroupRef decls) {
   for (auto decl = decls.begin(); decl != decls.end(); ++decl)
-    inliner.TraverseDecl(*decl);
+    inlineVisitor.TraverseDecl(*decl);
 
   return true;
 }
--- Inline/InlineASTConsumer.hh
+++ Inline/InlineASTConsumer.hh
@@ -2,17 +2,18 @@
 #define VYGRAPH_INLINEASTCONSUMER_HH
 
 #include "clang/AST/ASTConsumer.h"
-#include "clang/Rewrite/Core/Rewriter.h"
 
 #include "InlineASTVisitor.hh"
 
 namespace vy {
 
 class InlineASTConsumer : public clang::ASTConsumer {
-  InlineASTVisitor inliner;
 public:
   InlineASTConsumer(clang::Rewriter&, clang::ASTContext&);
   bool HandleTopLevelDecl(clang::DeclGroupRef) override;
+
+private:
+  InlineASTVisitor inlineVisitor;
 };
 
 } // namespace vy
